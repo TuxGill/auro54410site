e071b5b8a5a53a819804ae3462144a994f93c98c
teresa-bensaude <tux17@Teresa-Bensaude-Tux17.local>
2017-03-23 17:27:10 +0000

    list-news

--- components/content/view/list-news.php
+++ components/content/view/list-news.php
@@ -1,6 +1,33 @@
 <div class="slider">
-  <img src="images/produtos.jpg">
+  <img src="images/news.jpg">
   <div class="subtitle">
-    <p class="col-sm-6 offset-sm-1">OS NOSSOS <br>PRODUTOS</p>
+    <p class="col-sm-6 offset-sm-1">AS NOSSAS <br>NOTÍCIAS</p>
   </div>
+</div>
+
+<div class="row conteudo-news">
+
+		<div class="col-sm-4 offset-sm-2">
+			<img src="images/new1.jpg">
+		</div>
+		<div class="col-sm-4 textos">
+			<h2>SETEMBRO 03, 2016</h2>
+			<h3>
+				Lorem ipsum dolor sit amet, consectetur
+				adipiscing elit. Morbi rutrum diam vitae
+				nibh eleifend, et dictum est venenatis.
+				Proin dignissim eu mauris nec egestas.
+			</h3>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing
+				elit. Morbi rutrum diam vitae nibh eleifend, et dictum
+				est venenatis. Proin dignissim eu mauris nec egestas.
+				Sed imperdiet eget lectus quis interdum 
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+				Morbi rutrum diam vitae nibh eleifend, et dictum est
+				venenatis. Proin dignissim eu mauris nec..
+			</p>
+		</div>
+
+	
 </div>
