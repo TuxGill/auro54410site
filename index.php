ad603f2e94585fb35f04898cfee6532dc072a2db
hugotuxgill <Tux14@Hugo-Oliveira-Tux14.local>
2017-03-23 14:58:33 +0000

    alteração estrutura index

--- index.php
+++ index.php
@@ -52,44 +52,14 @@
             include('views/menu.php');
           ?>
         </div>
+        <?php
 
-        <div class="slider">
-          <img src="images/homeIMG.jpg">
-          <div class="subtitle">
-            <p class="col-sm-6 offset-sm-1">UM COMPROMISSO <br>PARA A VIDA</p>
-            <a class="col-sm-6 offset-sm-1" href="#">SABER MAIS</a>
-          </div>
-        </div>
-
-        <div class="row conteudo">
-          <div class="separador"><p>DESTAQUE</p></div>
-          <div class="col-sm-4 offset-sm-2">
-            <img src="images/produtos.png">
-          </div>
-          <div class="line">
-            <img src="images/line.svg">
-          </div>
-          
-          <div class="col-sm-2 texto">
-            <p class="title-destaque">DECUBAL</p>
-            <p class="txt-destaque">HIDRATAÇÃO <br>PROFUNDA <br>DA PELE</p>
-          </div>
-
-          <div class="line">
-            <img src="images/line.svg">
-          </div>
-             <!--  <span class="icon-stack">
-                <i class="fa fa-circle-thin icon-stack-3x"></i>
-                <i class="fa fa-angle-right icon-stack-1x"></i>
-<<<<<<< HEAD
-              </span> -->
-          <div class="seta">
-            <!-- <img src="images/seta.png"> -->
-          </div>
-
-
-
-        </div>
+        $route = explode('/',$_GET['area']);
+         $routeID = explode('/',$_GET['ID']);
+         print_r($route);
+         print_r($routeID); 
+         ?>
+        <?php include('views/home.php')?>
 
         <footer>
             <?php include('views/footer.php'); ?>
