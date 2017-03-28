68a7bdb8df5cefc5c8e50e9ed8d35928a445efd3
MariaCosta <maria.c@tux-gill.pt>
2017-03-28 18:36:53 +0100

    beacita-detail

--- components/products/view/detail-products.php
+++ components/products/view/detail-products.php
@@ -6,17 +6,20 @@
     frameborder="0"></iframe> -->
 
     <!-- TODO: BACKOFFICE - SE FOR MEDIA -->
-    <video src="media/decubalWeb.mp4" autoplay loop poster="images/poster_video_decubal.jpg">
+    <!-- <video src="media/decubalWeb.mp4" autoplay loop poster="images/poster_video_decubal.jpg"> -->
+    <video src="media/videos/beacitaWeb.mp4" autoplay loop poster="images/poster_video_decubal.jpg">
+    <!-- <video src="media/ferlidonaWeb.mp4" autoplay loop poster="images/poster_video_decubal.jpg"> -->
+
 
     </video>
   </div>
 </div>
-
-<div class="row conteudo conteudoProd col-sm-12">
+<!-- DECUBAL -->
+<div style="display:none" class="row conteudo conteudoProd col-sm-12">
   <div class="separador col-sm-1"><p>DECUBAL</p></div>
 
 	<div class="col-sm-8 offset-sm-1 txt-intro">
-    <img class="logoProd" src="images/logo_decubal.png" alt="">
+    <img class="logoProd" src="media/images/logo_decubal.png" alt="">
 
     <p>
 			De origens nórdicas, todos os produtos Decubal são desenvolvidos em estreita parceria
@@ -37,16 +40,15 @@
 		</p>
 
     <div class="gamaProd">
-      <img class="" src="images/decubal_gama_detail_products.png" alt="">
+      <img class="" src="media/images/decubal_gama_detail_products.png" alt="">
     </div>
 	</div>
-
   <div class="col-sm-12 linksprod">
       <p>ENTRE NO MUNDO DECUBAL</p>
       <div>
         <a target="_blank" href="http://decubal.pt/">
           <i class="fa fa-desktop" aria-hidden="true"></i>
-          <span>DECUBAL.PT</span>
+          DECUBAL.PT
         </a>
 
         <a target="_blank"  href="https://www.facebook.com/decubalportugal">
@@ -55,5 +57,31 @@
         </a>
       </div>
   </div>
+</div>
 
+
+<!-- BEACITA -->
+<div style="display:block" class="row conteudo conteudoProd col-sm-12">
+  <div class="separador col-sm-1"><p>BEACITA</p></div>
+
+	<div class="col-sm-8 offset-sm-1 txt-intro">
+    <img class="logoProd" src="media/images/logo_beacita.png" alt="">
+
+    <p>
+			Beacita é indicado para perda de peso em adultos com excesso de peso <br>
+      (índice de massa corporal, IMC, >= 28 kg/m2) e deve ser tomado em associação a uma dieta <br>
+      com baixo teor de calorias e gorduras.
+		</p>
+
+    <div class="gamaProd">
+      <img class="" src="media/images/beacita_gama_detail_products.png" alt="">
+    </div>
+	</div>
+  <div class="col-sm-12 rcmprod">
+        <a target="_blank" href="media/pdf/PDF_DE_EXEMPLO.pdf">
+          <i class="fa fa-file-text" aria-hidden="true"></i>
+          RCM
+        </a>
+      </div>
+  </div>
 </div>
