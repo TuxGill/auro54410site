87f364a60ba7d87b9f60d91032770b333cc9a32f
teresa-bensaude <tux17@Teresa-Bensaude-Tux17.local>
2017-03-30 18:27:44 +0100

    página - o grupo

---
+++ components/content/view/grupo.php
@@ -0,0 +1,30 @@
+<div class="slider">
+  <img src="<?php echo BASE_URL; ?>/media/images/grupo_slider.jpg">
+  <div class="subtitle">
+    <p class="col-sm-6 offset-sm-1 col-8 offset-1">UM GRUPO <br>FARMACÊUTICO GLOBAL</p>
+  </div>
+</div>
+
+<div class="row conteudo conteudo-grupo col-sm-12">
+	<div class="separador col-sm-1">O GRUPO</div>
+	<div class="col-sm-12 col-lg-12 col-xl-11">
+		<p>O Grupo Aurobindo detém uma estrutura total e verticalmente integrada
+		que oferece soluções farmacêuticas inovadoras, sendo uma das companhias
+		de topo na produção mundial de API (Active Pharmaceutical Ingredients).</p>
+	</div>
+
+	<div class="col-sm-12 col-lg-12 col-xl-11 offset-xl-1 ">
+		<ul>
+			<li><p>Presente em <em>37 países</em></p></li>
+			<li><p>Mais de <em>10,000 colaboradores em todo o mundo</em></p></li>
+			<li><p><em>Verticalmente integrada,</em>desde a I&ampD à comercialização</p></li>
+			<li><p>Exportação para <em>mais de 150 países</em></p></li>
+			<li><p>2 centros de <em>pesquisa e desenvolvimento</em></p></li>
+			<li><p><em>18 fábricas</em> com os mais recentes equipamentos</p></li>
+			<li><p><em>Mais de 15 mil toneladas</em> de princípios ativos farmacêuticos produzidos anualmente</p></li>
+			<li><p><em>Capacidade para produzir</em> formulações das mais convencionais às mais complexas</p></li>
+			<li><p><em>Instalações aprovadas</em> pelo INFARMED, USFDA, UKMHRA, Health Canada, MCC- South Africa,<br>
+	     		ANVISA – Brasil, TGA-Australia, GCC DR, etc.</p></li>
+		</ul>
+	</div>
+</div>
