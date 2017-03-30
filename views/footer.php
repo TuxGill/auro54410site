af29180b6c0757c7c3409ddc92530662298e67e7
MariaCosta <maria.c@tux-gill.pt>
2017-03-30 11:18:21 +0100

    footer responsive fix

--- views/footer.php
+++ views/footer.php
@@ -1,119 +1,116 @@
-<div class="col-sm-2 offset-sm-10 vermapa"><p>VER MAPA</p></div>
+<div class="col-sm-12 vermapa"><p>VER MAPA</p></div>
 <div class="row geral col-sm-12">
-<div class="col-sm-1 separador"><p>CONTACTOS</p></div>
-    <div class="col-sm-10 offset-sm-1 organizer">
-        <div class="row">
-              <ul class="col-md-5 col-xs-12">
-                  <li><h2>DADOS GERAIS</h2></li>
-                  <li class="info-item">
-                      <div class="icon"><i class="fa fa-home fa-2x" aria-hidden="true"></i></div>
-                      <div class="info">
-                        <ul>
-                            <li>Avenida do Forte</li>
-                            <li>Parque Suécia, Ed4, 2º piso</li>
-                            <li>2764-038 Carnaxide</li>
-                            <li>Portugal</li>
-                        </ul>
-                      </div>
-                  </li>
+    <div class="col-sm-1 separador"><p>CONTACTOS</p></div>
+    <div class="col-sm-11 row clearfix">
+      <ul class="col-md-6 col-xs-12 clearfix">
+          <li><h2>DADOS GERAIS</h2></li>
+          <li class="info-item">
+              <div class="icon"><i class="fa fa-home fa-2x" aria-hidden="true"></i></div>
+              <div class="info">
+                <ul>
+                    <li>Avenida do Forte</li>
+                    <li>Parque Suécia, Ed4, 2º piso</li>
+                    <li>2764-038 Carnaxide</li>
+                    <li>Portugal</li>
+                </ul>
+              </div>
+          </li>
 
-                  <li class="info-item">
+          <li class="info-item">
 
-                      <div class="icon"><i class="fa fa-envelope fa-2x" aria-hidden="true"></i></div>
-                      <div class="info">
-                        <ul>
-                            <li>Email Geral</li>
-                            <li><p>geral@aurovitas.pt</p></li>
-                        </ul>
-                      </div>
-                  </li>
+              <div class="icon"><i class="fa fa-envelope fa-2x" aria-hidden="true"></i></div>
+              <div class="info">
+                <ul>
+                    <li>Email Geral</li>
+                    <li><p>geral@aurovitas.pt</p></li>
+                </ul>
+              </div>
+          </li>
 
-                  <li class="info-item">
+          <li class="info-item">
 
-                      <div class="icon"><i class="fa fa-phone fa-2x" aria-hidden="true"></i></div>
-                      <div class="info">
-                        <ul>
-                            <li>+351 217 220 650</li>
-                        </ul>
-                      </div>
-                  </li>
+              <div class="icon"><i class="fa fa-phone fa-2x" aria-hidden="true"></i></div>
+              <div class="info">
+                <ul>
+                    <li>+351 217 220 650</li>
+                </ul>
+              </div>
+          </li>
 
-                  <li class="info-item">
+          <li class="info-item">
 
-                      <div class="icon"><i class="fa fa-fax fa-2x" aria-hidden="true"></i></div>
-                      <div class="info">
-                        <ul>
-                            <li>+351 217 272 744</li>
-                        </ul>
-                      </div>
-                  </li>
+              <div class="icon"><i class="fa fa-fax fa-2x" aria-hidden="true"></i></div>
+              <div class="info">
+                <ul>
+                    <li>+351 217 272 744</li>
+                </ul>
+              </div>
+          </li>
 
-                  <li class="info-item">
+          <li class="info-item">
 
-                      <div class="icon"><i class="fa fa-linkedin-square fa-2x" aria-hidden="true"></i></div>
-                      <div class="info">
-                        <ul>
-                            <li>Aurovitas Portugal</li>
-                        </ul>
-                      </div>
-                  </li>
+              <div class="icon"><i class="fa fa-linkedin-square fa-2x" aria-hidden="true"></i></div>
+              <div class="info">
+                <ul>
+                    <li>Aurovitas Portugal</li>
+                </ul>
+              </div>
+          </li>
 
 
-              </ul>
-              <ul class="col-md-5 col-xs-12">
-                <li><h2>APOIO AO CLIENTE</h2></li>
-                <li class="info-item">
+      </ul>
+      <ul class="col-md-6 col-xs-12 clearfix">
+        <li><h2>APOIO AO CLIENTE</h2></li>
+        <li class="info-item">
 
-                  <div class="icon"><i class="fa fa-envelope fa-2x" aria-hidden="true"></i></div>
-                  <div class="info">
-                    <ul>
-                        <li>Email Apoio a cliente Farmácias e Armazenistas
-                          <p>apoiocliente@aurovitas.pt</p>
-                        </li>
-                    </ul>
-                  </div>
+          <div class="icon"><i class="fa fa-envelope fa-2x" aria-hidden="true"></i></div>
+          <div class="info">
+            <ul>
+                <li>Email Apoio a cliente Farmácias e Armazenistas
+                  <p>apoiocliente@aurovitas.pt</p>
                 </li>
-                <li class="linha-separador"></li>
-                <li><h2>FARMACOVIGILÂNCIA</h2></li>
-                <li class="info-item">
-                     
-                      <div class="icon"><i class="fa" aria-hidden="true"></i></div>
-                      <div class="info">
-                        <ul>
-                          <li>
-                              Para pedir informações sobre os nossos
-                              medicamentos ou para qualquer assunto
-                              relacionado com farmacovigilância
-                              (ex: reações adversas) contactar:
-                          </li>
-                          
-                        </ul>
-                      </div>
-                </li>
-                <li class="info-item">
-                     
-                  <div class="icon"><i class="fa fa-envelope fa-2x" aria-hidden="true"></i></div>
-                  <div class="info">
-                    <ul>
-
-                      <li>Email farmacovigilância
-                          <p>pharmacovigilance.portugal@aurobindo.com</p>
-                      </li>
-                    </ul>
-                          </div>
-                    </li>
-
-              </ul>
+            </ul>
           </div>
-        </div>
+        </li>
+        <li class="linha-separador"></li>
+        <li><h2>FARMACOVIGILÂNCIA</h2></li>
+        <li class="info-item">
 
+              <div class="icon"><i class="fa" aria-hidden="true"></i></div>
+              <div class="info">
+                <ul>
+                  <li>
+                      Para pedir informações sobre os nossos
+                      medicamentos ou para qualquer assunto
+                      relacionado com farmacovigilância
+                      (ex: reações adversas) contactar:
+                  </li>
+
+                </ul>
+              </div>
+        </li>
+        <li class="info-item">
+
+          <div class="icon"><i class="fa fa-envelope fa-2x" aria-hidden="true"></i></div>
+          <div class="info">
+            <ul>
+
+              <li>Email farmacovigilância
+                  <p>pharmacovigilance.portugal@aurobindo.com</p>
+              </li>
+            </ul>
+                  </div>
+            </li>
+
+      </ul>
+    </div>
 </div>
 
-<div class="row rodape col-sm-12">
-  <div class="copyEsq">
+<div class="row col-sm-12 rodape">
+  <div class="copyEsq col-sm-6">
     <p>©Copyright 2016 Aurovitas</p>
   </div>
-  <div class="copyDir">
+  <div class="copyDir col-sm-6">
     <p>
       Site map | Termos do Site | Política de privacidade
     </p>
