3acab207b6b17b0bd8b6108ff936f0251fc432d3
teresa-bensaude <tux17@Teresa-Bensaude-Tux17.local>
2017-04-05 17:51:42 +0100

    submit-contact

--- components/contacts/views/admin/submit.contact.php
+++ components/contacts/views/admin/submit.contact.php
@@ -8,4 +8,22 @@
 	$farmacovigilancia = $_POST['farmacovigilancia'];
 
 	print_r($_POST);
+
+
+	/* CRIAR OBJECTO */
+
+	$contact=getContactById($pdo,$id);
+	
+
+	$contact[0]->setMorada($morada);
+	$contact[0]->setEmail ($email);
+	$contact[0]->setTelefone ($telefone);
+	$contact[0]->setFax ($fax);
+	$contact[0]->setLinkedin ($linkedin);
+	$contact[0]->setApoiocliente ($apoioCliente);
+	$contact[0]->setFarmacovigilancia ($farmacovigilancia);
+
+
+	
+	$contact[0]->update($pdo); 
 ?>
