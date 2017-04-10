a26ea09df9ac0cc921fac6b33b9f4824a9f8c2da
MariaCosta <maria.c@tux-gill.pt>
2017-04-10 19:03:21 +0100

    integração noticias - lista e detalhe

--- components/content/view/detail-news.php
+++ components/content/view/detail-news.php
@@ -1,25 +1,34 @@
+<?php
+//O get diz id mas é o slug
+ $slugNoticia = $_GET['id'];
+
+ $noticiaContent = getContentByCategorySlug($pdo, $slugNoticia);
+
+ ?>
 <div class="slider">
-  <img src="images/news-detail.jpg">
+  <img src="<?php echo BASE_URL.MEDIA_IMAGES.$noticiaContent[0]->getUrlImg(); ?>">
   <div class="subtitle">
-    <p class="col-sm-6 offset-sm-1">LOREM<br>IPSUM</p>
+    <p class="col-sm-6 offset-sm-1"><?php echo $noticiaContent[0]->getTitle(); ?></p>
   </div>
 </div>
 <div class="col-sm-12 conteudo">
-	<div class="col-sm-1 offset-sm-9 icon-news-link"><i class="fa fa-share-alt fa-lg"></i></div>
-	<h2 class="col-sm-8 offset-sm-2">OUTUBRO 27, 2016</h2>
-		
+	<div class="col-sm-1 offset-sm-9 icon-news-link">
+  <a name="fb_share"  href="https://www.facebook.com/sharer.php?u=<?php echo BASE_URL.'/detalhe-noticia/'.$slugNoticia?>&t=TEst">
+      <i class="fa fa-share-alt fa-lg"></i>
+    </a>
+  </div>
+	<h2 class="col-sm-8 offset-sm-2">
+    <?php
+        setlocale(LC_ALL, 'pt_PT');
+        echo strftime("%B %d, %Y", strtotime($noticiaContent[0]->getTs()) );
+      ?>
+	</h2>
+
 		<h3 class="col-sm-8 offset-sm-2">
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi rutrum diam vitae
-			nibh eleifend, et dictum est venenatis. Proin dignissim eu mauris nec egestas.
+			<?php echo $noticiaContent[0]->getIntro(); ?>
 		</h3>
-		
+
 		<p class="col-sm-8 offset-sm-2">
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi rutrum diam vitae nibh eleifend, et dictum est venenatis. Proin dignissim eu mauris nec egestas. Sed imperdiet eget lectus quis interdum. Curabitur nisl massa, semper a efficitur sed, efficitur varius diam. Morbi posuere nisl at tortor efficitur vehicula. Etiam mauris nunc, lacinia id libero quis, tempor malesuada libero. Praesent id orci gravida, luctus dolor et, mollis orci. Mauris blandit turpis non sem aliquet semper.
-			<br>
-			<br>
-			Suspendisse et velit sapien. Sed eleifend sit amet nisl eu feugiat. Vestibulum pharetra vestibulum tempus. Quisque viverra erat sed tellus condimentum, ac hendrerit mi pellentesque. Proin nisl lectus, faucibus ac augue id, hendrerit tempor metus. Praesent quis mi in lorem aliquam lobortis sed vel nibh. Suspendisse a justo ut mauris pellentesque scelerisque. Pellentesque malesuada varius urna, eget varius ipsum dapibus at. Morbi eget accumsan tellus. Morbi luctus luctus orci, at luctus metus.
-			<br>
-			<br>
-			Fusce et egestas erat. Phasellus efficitur dui vitae neque malesuada pharetra. Etiam et mi vitae sapien ultricies ultrices tempus vitae est. Donec odio sapien, dictum eget pulvinar sed, consectetur et augue. Maecenas congue fermentum magna nec lobortis. Sed faucibus turpis lobortis, iaculis nulla interdum, fermentum mi. Cras consequat accumsan condimentum. In aliquet semper dolor, eu aliquam augue molestie sit amet. Aliquam erat volutpat.
+			  <?php echo $noticiaContent[0]->getText(); ?>
 		</p>
 </div>
