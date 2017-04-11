4a1f64e58696615a93e166994ca4d730a368b65f
Maria-FC <maria.c@tux-gill.pt>
2017-04-11 17:39:22 +0100

    Class ProductCategory Updated (#15)
    * controller products updated
    
    * Class ProductCategory Updated
    
    acrescentamos label na  BD

--- components/products/controllers/Controller.ProductCategory.php
+++ components/products/controllers/Controller.ProductCategory.php
@@ -13,16 +13,19 @@
 
     foreach ($rows as $row) {
       $c= new ProductCategory($row['id_product_category'],
-       $row['fk_id_product_category'],
-        $row['title_product_category'],
-         $row['intro_product_category'],
-         $row['slug_product_category'],
-         $row['text_product_category'],
-         $row['url_product_category'],
-         $row['order_product_category'],
-          $row['act_product_category'],
-           $row['del_product_category'],
-           $row['ts_product_category']);
+                            $row['fk_id_product_category'],
+                            $row['title_product_category'],
+                            $row['label_product_category'],
+                            $row['intro_product_category'],
+                            $row['slug_product_category'],
+                            $row['text_product_category'],
+                            $row['url_product_category'],
+                            $row['order_product_category'],
+                            $row['act_product_category'],
+                            $row['del_product_category'],
+                            $row['ts_product_category']);
+
+
 
       array_push($product_categories, $c);
     }
@@ -30,28 +33,31 @@
     return $product_categories;
   }
 
+
   function getProductCategoryById($pdo,$id){
     $sql= "select * from product_category where id_product_category=".$id." and  del_product_category=0";
+
 
     $query = $pdo->prepare($sql);
     $query->execute();
 
     $rows = $query->fetchAll(PDO::FETCH_ASSOC);
+
     $product_categories=[];
 
     foreach ($rows as $row) {
-      $c= new ProductCategory($row['id_product_category'],
-       $row['fk_id_product_category'],
-         $row['title_product_category'],
-
-           $row['intro_product_category'],
-           $row['slug_product_category'],
-           $row['text_product_category'],
-           $row['url_product_category'],
-           $row['order_product_category'],
-            $row['act_product_category'],
-             $row['del_product_category'],
-             $row['ts_product_category']);
+        $c= new ProductCategory($row['id_product_category'],
+                              $row['fk_id_product_category'],
+                              $row['title_product_category'],
+                              $row['label_product_category'],
+                              $row['intro_product_category'],
+                              $row['slug_product_category'],
+                              $row['text_product_category'],
+                              $row['url_product_category'],
+                              $row['order_product_category'],
+                              $row['act_product_category'],
+                              $row['del_product_category'],
+                              $row['ts_product_category']);
 
       array_push($product_categories, $c);
     }
