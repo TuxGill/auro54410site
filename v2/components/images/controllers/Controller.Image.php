d2dbe7ccee5d8f7c37975f320adb78aef7d45c3e
hugotuxgill <Tux14@Hugo-Oliveira-Tux14.local>
2017-06-05 16:22:37 +0100

    pasta v2 removida

--- v2/components/images/controllers/Controller.Image.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-  include($_SERVER['DOCUMENT_ROOT'].DIRFOLDER.'/components/images/models/Class.Image.php');
-?>
--- v2/components/products/views/admin/del.product.php
+++
@@ -1,8 +0,0 @@
-<?php
-	include('../../../../config.php');
-	$p=getProductById($pdo, $_GET['id']);
-	//print_r($p);
-
-	$p[0]->delete($pdo);
-	header('Location:'.BASE_URL_BACKOFFICE.'//home.php?area=newproduct&idCat='.$p[0]->getCategory());
-?>
